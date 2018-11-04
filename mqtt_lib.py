9361059b080b9ec8d4b7cf7a32c184308af416ee
Eric Miller <eric@legoaces.org>
2018-11-04 00:10:00 -0400

    Added library for communications

---
+++ mqtt_lib.py
@@ -0,0 +1,54 @@
+import paho.mqtt.client as mqtt
+import random
+from time import sleep
+
+class Communicator(object):
+    def __init__(self):
+        self._channel = 'arming'
+        self._client = mqtt.Client('whack-{}'.format(random.randint(0, 100000)))
+        self.is_armed = False
+        self.last_msg = None
+
+        self._client.on_message = self._on_message
+        self._client.username_pw_set('yctweqrz', '7exM74SXFRjX')
+        self._client.connect("m15.cloudmqtt.com", 16481)
+        sleep(1.0)
+        self._client.subscribe(self._channel)
+
+        self._client.loop_start()
+
+        self._i = 0
+
+
+    def _on_message(self, client, userdata, message):
+        msg = str(message.payload.decode('utf-8'))
+        self.last_msg = msg
+        print("Message on MQTT", msg)
+        if 'Armed' in msg:
+            self.is_armed = True
+        elif 'Disarmed' in msg:
+            self.is_armed = False
+        else:
+            print("Unable to decipher message")
+
+    def send_message(self, switch_on:bool, people_present:bool):
+        msg = "{} {}".format('Armed' if (switch_on and people_present) else 'Disarmed', self._i)
+        self._i += 1
+        self._client.publish(self._channel, msg, qos=1)
+
+if __name__ == '__main__':
+    com = Communicator()
+
+    import sys
+    if sys.argv[1] == 'subscribe':
+        print('subscribing')
+        while True:
+            print(com.is_armed, com.last_msg)
+            sleep(1)
+    else:
+        print('publishing')
+        while True:
+            com.send_message(True, True)
+            sleep(2)
+            com.send_message(False, False)
+            sleep(2)
--- mqtt_test.py
+++ mqtt_test.py
@@ -2,21 +2,38 @@ import paho.mqtt.client as mqtt
 import random
 from time import sleep
 
-CHANNEL = 'whack/arm'
+CHANNEL = 'whack/switcharm'
 
-client = mqtt.Client('whack-switch')
-client.connect('broker.hivemq.com')
+client = mqtt.Client('whack-switch-{}'.format(random.randint(0, 1000)))
+# client.connect('broker.hivemq.com')
 
 def on_message(client, userdata, message):
     print(message.payload.decode("utf-8"))
 
+# def on_connect(client, userdata, rc):
+#     print('Connected')
+#     client.subscribe(CHANNEL)
+
 client.on_message = on_message
-client.loop_start()
+# client.on_connect = on_connect
 
-client.subscribe(CHANNEL, qos=2)
+client.username_pw_set('yctweqrz', '7exM74SXFRjX')
+# client.connect("broker.hivemq.com")
+client.connect("m15.cloudmqtt.com", 16481)
+
 sleep(1)
+client.subscribe(CHANNEL)
 
-client.publish(CHANNEL, "Armed {}".format(random.randint(0,1000)), qos=1)
+import sys
+if sys.argv[1] == 'subscribe':
+    print('subscribing')
+    client.loop_forever()
+else:
+    print('publishing')
+    msg = "Armed {}".format(random.randint(0,1000))
+    print(msg)
+    client.publish(CHANNEL, msg, qos=1)
 
-sleep(5)
-client.loop_stop()
+    # client.loop_start()
+    sleep(2)
+    # client.loop_stop()
